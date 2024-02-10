88766bdc964c30c49d4070ca3ad130dd7c8eeab5
Aleksa Djordjiev <aleksa.kb24@live.com>
2024-02-10 12:58:00 +0100

    Sidebar dodat

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,29 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <>
+      <div className='home-container'>
+        <div className='home-tile'>
+          <h1></h1>
+          <h2>Upita u poslednjih 7 dana</h2>
+        </div>
+        <div className='home-tile'>
+          <h1></h1>
+          <h2>Upita sa rokom u sledecih 7 dana</h2>
+        </div>
+        <div className='home-tile'>
+          <h1></h1>
+          <h2>Napravljenih kalkulacija u poslednjih 7 dana</h2>
+        </div>
+        <div className='home-tile'>
+        </div>
+        <div className='home-tile'>
+        </div>
+        <div className='home-tile'>
+        </div>
+      </div>
+      <button className='novi-upit'>+</button>
+    </>
   )
 }
 
