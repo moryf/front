af5152636d4bf3ea0858d64e67d5b4560bc0b96c
Moryf <aleksa.kb24@gmail.com>
2024-07-17 10:51:37 +0200

    Update AxiosConfig.js with new base URL for API requests

--- src/api/axios/AxiosConfig.js
+++ src/api/axios/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://157.90.232.183:9090',
     auth: {
         username: 'konstil_joilart',
         password: 'konstil2024'
